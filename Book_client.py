53bee2c001a04b860380c20fe8d68b9ad8744326
JeongBin <jjb080319@gmai.com>
2022-05-23 10:30:51 +0900

    first

--- Book_client.py
+++ Book_client.py
@@ -1,8 +1,14 @@
 import sqlite3
 con = sqlite3.connect('Book.db')
 
-con.execute("SELECT * FROM Books")
+c = con.cursor()
 
-print(con)
+c.execute("SELECT signature FROM Books")
 
+row = c.fetchone()
+while row:
+    print("이름:",c.fetchmany(size=1))
+    row = c.fetchone()
+
+c.close()
 con.close()
